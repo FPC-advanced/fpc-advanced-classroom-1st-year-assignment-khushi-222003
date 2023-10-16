9d4651465b773312ed2f8328a5bda62a4728781a
khushi-222003 <khushik.bsc23@rvu.edu.in>
2023-10-16 10:54:48 +0530

    committed 10/16/2023, 10:54:48 AM

--- set01/problem09.c
+++ set01/problem09.c
@@ -1,30 +1,30 @@
 #include<stdio.h>
 #include<math.h>
-    float input(){
-        float num;
-        printf("Enter the number");
-        scanf("%f",&num);
-        return num;
+double input(){
+double num;
+printf("Enter the number\n");
+scanf("%lf",&num);
+return num;
+}
+ double mysqrt(double x){
+    double y=x;
+    double z=(y+(x/y))/2;
+    while(fabs(y-z)>=0.000001){
+        y=z;
+        z=(y+(x/y))/2;
     }
-    float mysqrt(float a){
-        float pguess=a/2;
-        float next=(pguess+a/pguess)/2;
-        while(fabs(next*next-a)<0.000001){
-            pguess=next;
-            next=(pguess+a/pguess)/2;
-            return next;
-        }
-        
-
-    }
-    void output(float a,float root){
-        printf("The squareroot of %f is %f\n",a,root);
-
-    }
-    int main(){
-        float number=input();
-        float result=
-    }
+    return z;
     
+     }
+     void output(double x, double root){
+        printf("The square of %lf",root);
 
-    
+     }
+     int main(){
+        double num,x,root;
+        num=input();
+        double result=mysqrt(num);
+        output(num,result);
+        return 0;
+
+     }
