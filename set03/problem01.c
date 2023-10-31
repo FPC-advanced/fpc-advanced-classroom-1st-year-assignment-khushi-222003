c7f213b9412b19931e32e83c4e19be077f21b49d
khushi-222003 <khushik.bsc23@rvu.edu.in>
2023-10-31 11:10:48 +0530

    committed 10/31/2023, 11:10:48 AM

--- set03/problem01.c
+++ set03/problem01.c
@@ -5,4 +5,30 @@
 void input(float *x1, float *y1, float *x2, float *y2);
 float find_distance(float x1, float y1, float x2, float y2);
 void output(float x1, float y1, float x2, float y2, float distance);*/
-```
+#include<stdio.h>
+#include<math.h>
+float input(float *x1, float *y1, float *x2, float *y2){
+    printf("Enter the first cordinates\n", (x1,y1));
+    scanf("%f,%f",x1, y1);
+    printf("Enter the second cordinates\n", (x2,y2));
+    scanf("%f,%f",x2, y2);
+}
+
+float find_distance(float x1 ,float y1, float x2,  float y2){
+
+  float distance;
+  distance=sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+  return distance;
+
+}
+
+void output(float x1, float y1, float x2, float y2, float distance){
+  printf("The distance between (%f,%f)and (%f,%f)is %f\n",x1,y1,x2,y2,distance);
+}
+int main(){
+  float a,b,c,d,distance;
+  input(&a ,&b, &c, &d);
+  distance=find_distance(a,b,c,d);
+  output(a,b,c,d,distance);
+return 0;
+}
--- set03/problem02.c
+++ set03/problem02.c
@@ -6,3 +6,35 @@ void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float
 int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3);
 void output(float x1, float y1, float x2, float y2,float x3, float y3, int result);
 ```*/
+
+#include<stdio.h>
+void input(float *x1,float*y1, float *x2, float*y2, float *x3, float *y3){
+    printf("Enter the first points\n", (x1,y1));
+    scanf("%f,%f",x1,y1);
+    printf("ENter the second points\n");
+    scanf("%f,%f", x2,y2);
+    printf("Enter the third points\n");
+    scanf("%f, %f",x3,y3);
+}
+int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3){
+    if((x1!=x2|| y1!=y2)&&(x1!=x3||y1!=y3)&&(x2!=x3||y2!=y3)){
+        return 1;
+    }
+    return -1;
+
+}
+void output(float x1, float y1, float x2, float y2,float x3, float y3, float result){
+    if(result>0){
+        printf("The points %f,%f,%f,%f,%f,%f forms a triangle", x1,y1,x2,y2,x3,y3);
+    }
+    else{
+        printf("The points %f,%f,%f,%f,%f,%f do not form a triangle", x1,y1,x2,y2,x3,y3);
+    }
+}
+int main(){
+    float x1,y1,x2,y2,x3,y3,result;
+    input(&x1, &y1, &x2, &y2, &x3, &y3);
+    result=is_triangle(x1,y1,x2,y2,x3,y3);
+    output(x1,y1,x2,y2,x3,y3,result);
+    return 0;
+}
